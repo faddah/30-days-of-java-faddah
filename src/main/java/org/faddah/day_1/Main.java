25bd44a6e6f833eeec75dcf8f4af6f45160821cb
Faddah Wolf <my_biz@me.com>
2025-12-13 10:29:32 -0800

    Package creation and Folder structure walkthrough.

---
+++ src/main/java/org/faddah/day_1/Main.java
@@ -0,0 +1,9 @@
+package org.faddah.day_1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    void main() {
+        System.out.println("\n\n\t\tHello Amnigoscode‼️ 🎅\n");
+    }
+}
