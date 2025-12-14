0686799fd66c04df35fbe3ddf8630d803b3fac8c
Faddah Wolf <my_biz@me.com>
2025-12-14 11:46:59 -0800

    Added Exercise Java class file to Day 3 package.

--- src/main/java/org/faddah/day_3/Exercise3_1.java
+++ src/main/java/org/faddah/day_3/Exercise3_1.java
@@ -1,3 +1,15 @@
-void main() {
-    System.out.println("\n\n\t\tDay 3 Exercise 1\n");
+void main(String[] args) {
+    String text = "Java Programming is Fun!";
+
+    System.out.println("\n\t\t\t=== String Analysis ===");
+    System.out.println("\t\tOriginal text: " + text);
+    System.out.println("\t\tLength: " + text.length());
+    System.out.println("\t\tFirst character: " + text.charAt(0));
+    System.out.println("\t\tLast character: " + text.charAt(text.length() - 1));
+    System.out.println("\t\tUppercase: " + text.toUpperCase());
+    System.out.println("\t\tLowercase: " + text.toLowerCase());
+    System.out.println("\t\tContains 'Java': " + text.contains("Java"));
+    System.out.println("\t\tStarts with 'Java': " + text.startsWith("Java"));
+    System.out.println("\t\tEnds with 'Fun!': " + text.endsWith("Fun!") + "\n");
 }
+  
