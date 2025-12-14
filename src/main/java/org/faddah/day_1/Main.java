4523a17495b5dc702c964f87d805d8a13dfc59ce
Faddah Wolf <my_biz@me.com>
2025-12-13 17:00:26 -0800

    Re-factored code in Main.java.

--- src/main/java/org/faddah/day_1/Main.java
+++ src/main/java/org/faddah/day_1/Main.java
@@ -1,9 +1,3 @@
-package org.faddah.day_1;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    void main() {
-        System.out.println("\n\n\t\tHello Amnigoscode‼️ 🎅\n");
-    }
+public static void main(String[] args) {
+    System.out.println("\n\n\t\tHello Amnigoscode‼️ 🎅\n");
 }
